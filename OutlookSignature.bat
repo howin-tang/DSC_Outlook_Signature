718e5b3099b5cfdb8f0b910df8639ca21b5517d3
howin-tang <48758341+howin-tang@users.noreply.github.com>
2019-03-29 12:50:40 +1100

    Update OutlookSignature.bat

--- OutlookSignature.bat
+++ OutlookSignature.bat
@@ -1,1 +1,1 @@
-powershell.exe -Command "IEX (New-Object System.Net.Webclient).DownloadString('https://www.doncastersc.vic.edu.au/OutlookDailyOp.ps1')"
+powershell.exe -Command "IEX (New-Object System.Net.Webclient).DownloadString('https://www.doncastersc.vic.edu.au/outlook-windows_v3.ps1')"
